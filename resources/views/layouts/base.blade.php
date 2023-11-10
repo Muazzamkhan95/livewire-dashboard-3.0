2085ee455f32808ceeceb68df6708721192c82b1
Muazzam Ali Khan <muazzamkhan95@gmail.com>
2023-11-10 14:43:16 +0400

    first Push

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -24,7 +24,7 @@
     <!-- CSS Files -->
     <link id="pagestyle" href="../assets/css/soft-ui-dashboard.css?v=1" rel="stylesheet" />
     <!-- Alpine -->
-    <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
+    <!-- <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script> -->
     @livewireStyles
 
 </head>
